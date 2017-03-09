9eba0cf7f2c0df0df0dbaff294ad17540e8d769a
abir <izatop@gmail.com>
2017-03-09 18:59:32 +0300

    Tests fix

--- reader.spec.ts
+++ reader.spec.ts
@@ -14,24 +14,24 @@ test('Reader', async t => {
         IsBindable: undefined,
         IsSideEffecting: undefined,
         Name: 'GetProductsByRating',
-        Parameter: [{Name: 'rating', Type: 'Int16'}],
+        Parameter: [{Name: 'rating', Type: 'Edm.Int16'}],
         ReturnType: 'Collection(ODataDemo.Product)'
     });
 
     t.same(res.EnumType[0], {
         Name: 'RGBColors',
-        UnderlyingType: 'Int32',
+        UnderlyingType: 'Edm.Int32',
         Member: [{Name: 'Red'}, {Name: 'Green'}, {Name: 'Blue'}]
     });
 
     t.same(res.ComplexType[0], {
             Name: 'Address',
             Property: [
-                {Name: 'Street', Nullable: undefined, Type: 'String'},
-                {Name: 'City', Nullable: undefined, Type: 'String'},
-                {Name: 'State', Nullable: undefined, Type: 'String'},
-                {Name: 'ZipCode', Nullable: undefined, Type: 'String'},
-                {Name: 'Country', Nullable: undefined, Type: 'String'}
+                {Name: 'Street', Nullable: undefined, Type: 'Edm.String'},
+                {Name: 'City', Nullable: undefined, Type: 'Edm.String'},
+                {Name: 'State', Nullable: undefined, Type: 'Edm.String'},
+                {Name: 'ZipCode', Nullable: undefined, Type: 'Edm.String'},
+                {Name: 'Country', Nullable: undefined, Type: 'Edm.String'}
             ]
         }
     );
@@ -42,13 +42,13 @@ test('Reader', async t => {
         OpenType: undefined,
         BaseType: undefined,
         Property: [
-            {Name: 'ID', Nullable: false, Type: 'Int32'},
-            {Name: 'Name', Nullable: undefined, Type: 'String'},
-            {Name: 'Description', Nullable: undefined, Type: 'String'},
-            {Name: 'ReleaseDate', Nullable: false, Type: 'DateTime'},
-            {Name: 'DiscontinuedDate', Nullable: undefined, Type: 'DateTime'},
-            {Name: 'Rating', Nullable: false, Type: 'Int16'},
-            {Name: 'Price', Nullable: false, Type: 'Double'}
+            {Name: 'ID', Nullable: false, Type: 'Edm.Int32'},
+            {Name: 'Name', Nullable: undefined, Type: 'Edm.String'},
+            {Name: 'Description', Nullable: undefined, Type: 'Edm.String'},
+            {Name: 'ReleaseDate', Nullable: false, Type: 'Edm.DateTime'},
+            {Name: 'DiscontinuedDate', Nullable: undefined, Type: 'Edm.DateTime'},
+            {Name: 'Rating', Nullable: false, Type: 'Edm.Int16'},
+            {Name: 'Price', Nullable: false, Type: 'Edm.Double'}
         ],
         NavigationProperty: [{
             FromRole: 'Product_Categories',
@@ -70,5 +70,5 @@ test('Reader', async t => {
 
     t.same(res.EntitySet[0], {EntityType: 'ODataDemo.Product', Name: 'Products'});
 
-    t.same(res.Association[0], {End: [{Multiplicity: '*', Role: 'Category_Products', Type: 'Category' }, {Multiplicity: '*', Role: 'Product_Categories', Type: 'Product'}], Name: 'Product_Categories_Category_Products'});
+    t.same(res.Association[0], {End: [{Multiplicity: '*', Role: 'Category_Products', Type: 'ODataDemo.Category' }, {Multiplicity: '*', Role: 'Product_Categories', Type: 'ODataDemo.Product'}], Name: 'Product_Categories_Category_Products'});
 });
